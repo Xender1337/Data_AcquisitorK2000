31d43050f70f206c8ea06eef8a5c7dc57c8c3fcc
mbesson <mickabesson@gmail.com>
2017-09-04 05:39:09 +0200

    Add polling when measure and delay

--- DataAcquisitorK2000/script.py
+++ DataAcquisitorK2000/script.py
@@ -1,16 +1,35 @@
 from Library.k2000_driver import K2000Driver
 import datetime
+import sys
+
+SERIAL_INTERFACE = '/dev/serial/by-path/platform-3f980000.usb-usb-0:1.2:1.0-port0'
 
 test = K2000Driver()
 
+
+test.init_serial(SERIAL_INTERFACE)
+
+# delay = 0.02505265 / NPLC = 1 for ~0.05 sec per measurement
+# test.init_k2000(smpl_nbr=1024, trg_source="BUS", nplc="1", delay=0.02505265)
+test.init_k2000(smpl_nbr=1024, trg_source="BUS", nplc="0.1")
+
+test.trig_k2000(wait_acquisition=False)
+
 start = datetime.datetime.now()
 
-test.init_serial('/dev/serial/by-path/platform-3f980000.usb-usb-0:1.2:1.0-port0')
-test.init_k2000()
-test.trig_k2000()
-rslt = test.get_data()
+print("Execute Measurement")
+while True:
+    if int(test.get_status()) == 1024:
+        print
+        break
+    else:
+        sys.stdout.write('.')
+        sys.stdout.flush()
 
 end = datetime.datetime.now() - start
+
+rslt = test.get_data()
+
 
 # Print raw value from RS232
 # print(rslt)
@@ -21,7 +40,7 @@ data = rslt.split(",")
 index = 0
 
 for a_data in data:
-    test.decode_data(a_data)
+    #print(test.decode_data(a_data))
     index += 1
 
 print("Have {} value in this test.".format(index))
